b3622e47aa09d7ea48c5d5dfa7b2ed2f450bff0c
JuanLiviero <55903073+juanliviero@users.noreply.github.com>
2022-03-17 15:35:43 -0300

    AddEvents

---
+++ src/config/instances.js
@@ -0,0 +1,12 @@
+
+// User instances
+
+import User from '../models/user.js';
+
+export const users = new User('','');
+
+// Event instances
+
+import Event from '../models/event.js';
+
+export const events = new Event('','');
