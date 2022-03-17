949d0726dd2e7160c15360b6bb7b38eda97347a5
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-17 18:01:15 -0300

    Fix isAuthenticated.js

--- src/middlewares/isAuthenticated.js
+++ src/middlewares/isAuthenticated.js
@@ -1,10 +1,20 @@
+import { validateToken } from '../tools/token.js';
+
 export default function isAuthenticated(req, res, next){
-	if(req.headers.token){
-		next();
-	}else{
-		res.status(401).send({
+	try {
+		if(validateToken(req.headers.token)){
+			next();
+		}else{
+			res.status(401).send({
+				error: 'Unauthorized',
+				message: 'client failed to authenticate with the server'
+			});
+		}
+	} catch(error) {
+		res.status(409).send({
 			error: 'Unauthorized',
-			message: 'client failed to authenticate with the server'
+			message: error
 		});
 	}
+
 }
