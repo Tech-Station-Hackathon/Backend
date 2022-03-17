de0607223422bd3e332e397e43cdfb70821ac667
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-17 11:12:21 -0300

    Add 404 to routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,11 @@ routes.use('/', (req, res) => {
 	res.send('Hello World!');
 });
 
+routes.get('*', function(req, res){
+	res.status(404).send({
+		error: 'Not Found',
+		message: 'the requested resource does not exist'
+	});
+});
+
 export default routes;
