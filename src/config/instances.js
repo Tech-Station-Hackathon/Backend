53a558ddef95f350b0e303accccb2e5426c3635b
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-17 16:05:49 -0300

    Use dotenv in instances.js Add .env.example

--- src/config/instances.js
+++ src/config/instances.js
@@ -1,5 +1,6 @@
 import Event from '../models/event.js';
 import User from '../models/user.js';
+import 'dotenv/config';
 
-export const events = new Event('');	// Event Manager Instance
-export const users = new User('');		// User Manager Instance
+export const events = new Event(process.env.MONGO_URL);	// Event Manager Instance
+export const users = new User(process.env.MONGO_URL);		// User Manager Instance
