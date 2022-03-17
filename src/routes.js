982d8dde26edf8c65eb40b35714f363bb8a6af90
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-17 20:21:51 -0300

    use eventRouter

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import eventsRouter from './routers/eventsRouter.js';
 import usersRouter from './routers/usersRouter.js';
 
 const routes = express.Router();
 
 routes.use('/api/users/', usersRouter);
+routes.use('/api/events/', eventsRouter);
 
 routes.get('*', function(req, res){
 	res.status(404).send({
