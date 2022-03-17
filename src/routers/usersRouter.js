6b6814df7154afb99e4df405c7df3fefc7a96284
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-17 15:42:38 -0300

    Add routers in usersRouter.js

--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,15 +1,60 @@
 import express from 'express';
-import {users} from '../config/instances.js';
+import isAuthenticated from '../middlewares/isAuthenticated.js';
+import { users } from '../config/instances.js';
 
 const usersRouter = express.Router();
 
-usersRouter.get('/', async (req, res) => {
+usersRouter.get('/', isAuthenticated, async (req, res) => {
 	try{
 		let usersList = await users.getAllUsers();
 		res.send(usersList);
 	}
-	catch {
-		res.status(502).send();
+	catch (error) {
+		res.status(500).send({
+			error: 'Internal Server Error',
+			message: error
+		});
+	}
+});
+
+usersRouter.post('/register', async (req, res) => {
+	try{
+		let newUser = await users.addUser(
+			req.body.name,
+			req.body.lastname,
+			req.body.age,
+			req.body.avatar,
+			req.body.email,
+			req.body.password
+		);
+		res.send(newUser);
+	}
+	catch (error) {
+		res.status(500).send({
+			error: 'Internal Server Error',
+			message: error
+		});
+	}
+});
+
+usersRouter.post('/auth',async (req, res) => {
+	try{
+		if(await users.checkUser(req.body.email,req.body.password)){
+			const user = await users.getUserByEmail(req.body.email);
+			const token = user[0]._id;
+			res.send({token});
+		}else{
+			res.status(401).send({
+				error: 'Internal Server Error',
+				message: 'Incorrect username and password'
+			});
+		}
+	}
+	catch (error) {
+		res.status(500).send({
+			error: 'Internal Server Error',
+			message: error
+		});
 	}
 });
 
