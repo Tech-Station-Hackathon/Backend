fd5da18cb8df7abb3d44af7adb9e7f576efee41c
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-17 14:41:42 -0300

    add usersRouter.js in routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import usersRouter from './routers/usersRouter.js';
 
 const routes = express.Router();
 
-routes.use('/', (req, res) => {
-	res.send('Hello World!');
-});
+routes.use('/api/users/', usersRouter);
 
 routes.get('*', function(req, res){
 	res.status(404).send({
