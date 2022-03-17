f52ec368c4ab3f154976d2ae2a6f187cdcf2ee79
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-17 20:21:16 -0300

    Fix eventsRouter.js

--- src/routers/eventsRouter.js
+++ src/routers/eventsRouter.js
@@ -1,16 +1,60 @@
-// import events
-
-import {events} from '../config/instances.js';
+import { events } from '../config/instances.js';
+import express from 'express';
 
 const eventsRouter = express.Router();
 
 eventsRouter.get('/', async (req, res) => {
 	try{
-		let eventList = await users.getAllEvents();
+		let eventsList = await events.getAllEvents();
 		res.send(eventsList);
+	} catch (error) {
+		res.status(500).send({
+			error: 'Internal Server Error',
+			message: error
+		});
 	}
-	catch {
-		res.status(502).send();
+});
+
+eventsRouter.post('/', async (req, res) => {
+	try{
+		await events.addEvent(
+			req.body.title,
+			req.body.description,
+			req.body.stream,
+			req.body.date,
+			req.body.isOnline,
+			req.body.isPresential
+		);
+		res.send();
+	} catch (error){
+		res.status(500).send({
+			error: 'Internal Server Error',
+			message: error
+		});
+	}
+});
+
+eventsRouter.get('/presentials', async (req, res) => {
+	try{
+		let eventsList = await events.getAllEvents();
+		res.send(eventsList.filter(event => event.isPresential));
+	} catch(error) {
+		res.status(500).send({
+			error: 'Internal Server Error',
+			message: error
+		});
+	}
+});
+
+eventsRouter.get('/online', async (req, res) => {
+	try{
+		let eventsList = await events.getAllEvents();
+		res.send(eventsList.filter(event => event.isOnline));
+	} catch(error) {
+		res.status(500).send({
+			error: 'Internal Server Error',
+			message: error
+		});
 	}
 });
 
