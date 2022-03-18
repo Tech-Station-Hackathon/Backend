41be3a869485c078ed1fe85c8713cbd2a741818a
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-18 11:13:04 -0300

    Fix eventsubscriptionsRouter.js Add routers in routes.js

--- src/routers/eventsubscriptionsRouter.js
+++ src/routers/eventsubscriptionsRouter.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { eventsubscriptions } from '../config/instances.js';
+import {getIDInSession} from '../tools/token.js';
 
 const eventsubscriptionsRouter = express.Router();
 
@@ -7,6 +8,23 @@ eventsubscriptionsRouter.get('/:eventId', async (req, res) => {
 	try{
 		let eventSubscriptionList = await eventsubscriptions.getEventSubscriptionByID(req.params.eventId);
 		res.send(eventSubscriptionList);
+	}
+	catch (error){
+		res.status(500).send({
+			error: 'Internal Server Error',
+			message: error
+		});
+	}
+});
+
+eventsubscriptionsRouter.post('/', async(req, res) =>{
+	try{
+		await eventsubscriptions.addEventSubscription(
+			req.body.eventId,
+			getIDInSession(req.headers.token),
+			req.body.accessType
+		);
+		res.send();
 	}
 	catch (error){
 		res.status(500).send({
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import eventsRouter from './routers/eventsRouter.js';
+import eventsubscriptionsRouter from './routers/eventsubscriptionsRouter.js';
+import SuverysRouter from './routers/suverysRouter.js';
 import usersRouter from './routers/usersRouter.js';
 
 const routes = express.Router();
 
 routes.use('/api/users/', usersRouter);
 routes.use('/api/events/', eventsRouter);
+routes.use('/api/suverrys/', SuverysRouter);
+routes.use('/api/subscriptions/', eventsubscriptionsRouter);
 
 routes.get('*', function(req, res){
 	res.status(404).send({
